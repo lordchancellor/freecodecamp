459d6748ee54bb170568498024f00d6d8418bfc5
Christopher Ford <c.d.j.ford@gmail.com>
2016-01-21 21:44:17 +0000

    Quote generation JS added, basic page structure put together and initial styling applied

--- frontend-projects/quote-generator/js/main.js
+++ frontend-projects/quote-generator/js/main.js
@@ -1,51 +1,61 @@
 var quotes = [
     {
-        quote: "",
-        cite: ""
+        quote: "That's my Bad Mother F**ker",
+        cite: "Jules Winnfield"
     },
 
     {
-        quote: "",
-        cite: ""
+        quote: "The ability to destroy a planet is insignificant next to the power of the Force",
+        cite: "Darth Vader"
     },
 
     {
-        quote: "",
-        cite: ""
+        quote: "Time is the fire in which we burn",
+        cite: "Dr. Soran"
     },
-    
+
     {
-        quote: "",
-        cite: ""
+        quote: "Oh no you did not shoot that green sh*t at me",
+        cite: "Captain Steven Hiller"
     },
-    
+
     {
-        quote: "",
-        cite: ""
+        quote: "You guys might not know this, but I consider myself a bit of a loner. I tend to think of myself as a one-man wolf pack",
+        cite: "Alan Garner"
     },
-    
+
     {
-        quote: "",
-        cite: ""
+        quote: "Oh boy",
+        cite: "Sam Beckett"
     },
-    
+
     {
-        quote: "",
-        cite: ""
+        quote: "This sh*t'll make you into a goddamn sexual Tyrannosaurus",
+        cite: "Blain"
     },
-    
+
     {
-        quote: "",
-        cite: ""
+        quote: "He's got an arm off!",
+        cite: "Ed"
     },
-    
+
     {
-        quote: "",
-        cite: ""
+        quote: "If you can dodge a wrench, you can dodge a ball",
+        cite: "Patches O'Houlihan"
     },
-    
+
     {
-        quote: "",
-        cite: ""
+        quote: "I say we get drunk, cause I'm all out of ideas",
+        cite: "Metatron"
     }
 ];
+
+function quoteMe() {
+    var random = Math.floor(Math.random() * quotes.length);
+
+    var quote = quotes[random].quote;
+    var cite = quotes[random].cite;
+
+    $('#quote').text(quote);
+    $('#cite').text(cite);
+}
