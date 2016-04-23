0258a3b8cfa7021b385c7e25b4537dfd119f25c1
Christopher Ford <christopher@ChristohersMini.home>
2016-04-23 16:22:10 +0100

    booWho.js - Determining if a given argument is a Boolean object

---
+++ intermediate-algorithms/booWho.js
@@ -0,0 +1,15 @@
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+*/
+
+function booWho(bool) {
+    if (bool === Boolean(bool)) {
+        return true;
+    }
+
+    return false;
+}
+
+booWho(null);
