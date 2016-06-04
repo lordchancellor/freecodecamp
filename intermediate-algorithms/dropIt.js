a38a419b64d806ae612bbfa9e58c90df30569a4a
lordchancellor <c.d.j.ford@gmail.com>
2016-06-04 15:24:53 +0100

    dropIt.js - Dropping values from an array that do not meet the criteria of a passed function

---
+++ intermediate-algorithms/dropIt.js
@@ -0,0 +1,27 @@
+/*
+Drop the elements of an array (first argument), starting from the front, until the predicate 
+(second argument) returns true.
+
+The second argument, func, is a function you'll use to test the first elements of the array 
+to decide if you should drop it or not.
+
+Return the rest of the array, otherwise return an empty array.
+*/
+
+
+function dropElements(arr, func) {
+  for (var i = 0; i < arr.length; i++) {
+	if (!func(arr[i])) {
+		arr.splice(i, 1);
+		i--;
+	}
+	else {
+		console.log("Returning " + arr);
+		return arr;
+	}
+  }
+
+  return arr;
+}
+
+dropElements([1, 2, 3, 4], function(n) {return n >= 3; });
